01b5ece7c94e2437af03bb0102d44e4ca381c159
Charlotte Curtis <c.f.curtis@gmail.com>
2020-12-03 16:52:47 -0700

    Updating language files, adding Dutch and Spanish

--- update-loc.bat
+++ update-loc.bat
@@ -1,4 +1,6 @@
 pybabel extract -o locale/sewingutils.pot --copyright-holder="Charlotte Curtis" --project=sewingutils .
 pybabel update -D sewingutils -d locale -i locale/sewingutils.pot -l fr_FR
 pybabel update -D sewingutils -d locale -i locale/sewingutils.pot -l de_DE
+pybabel update -D sewingutils -d locale -i locale/sewingutils.pot -l nl_NL
+pybabel update -D sewingutils -d locale -i locale/sewingutils.pot -l es_ES
 pybabel compile -D sewingutils -d locale
