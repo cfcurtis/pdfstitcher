d387511326b24c02a1847784d6b3f693ada034e6
Charlotte Curtis <c.f.curtis@gmail.com>
2020-12-15 09:31:57 -0700

    Changed try/except on _MEIPASS to cleaner check.

--- pdfstitcher.py
+++ pdfstitcher.py
@@ -1,6 +1,7 @@
 import wx
 from tile_pages import *
 import os
+import sys
 
 # localization stuff
 import gettext
@@ -8,11 +9,11 @@ import locale
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
+    if hasattr(sys,'_MEIPASS'):
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
+    else:
+        base_path = os.path.abspath('.')
 
     return os.path.join(base_path, relative_path)
 
