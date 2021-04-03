73e8dc141843a593f60453a6793c4de85716daca
Charlotte Curtis <c.f.curtis@gmail.com>
2021-04-02 19:41:30 -0600

    Updating locale templates

--- update-loc.bat
+++ update-loc.bat
@@ -1,4 +1,4 @@
-pybabel extract -o locale\pdfstitcher.pot --copyright-holder="Charlotte Curtis" --project=pdfstitcher .
+pybabel extract -F babel.cfg -o locale\pdfstitcher.pot --copyright-holder="Charlotte Curtis" --project=pdfstitcher .
 FOR %%L IN (fr_FR,de_DE,nl_NL,es_ES) DO (
     if exist locale\%%L\LC_MESSAGES\pdfstitcher.po (
         pybabel update -D pdfstitcher -d locale -i locale\pdfstitcher.pot -l %%L
