81a1ddf033eca2522de6baf6d75d503029ed3e6d
Charlotte Curtis <c.f.curtis@gmail.com>
2021-05-26 12:55:23 -0600

    Modifying header to reflect MPL license. Closes #72.

--- layerfilter.py
+++ layerfilter.py
@@ -1,18 +1,9 @@
 # PDFStitcher is a utility to work with PDF sewing patterns.
 # Copyright (C) 2021 Charlotte Curtis
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 from os import write
 import pikepdf
