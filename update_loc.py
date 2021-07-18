389dcd92e4f597abb8ee3ffd7c8cf95a4dc29d46
Charlotte Curtis <c.f.curtis@gmail.com>
2021-07-18 14:13:35 -0600

    Modifying update_loc.py to allow for only one action at a time

--- update_loc.py
+++ update_loc.py
@@ -1,25 +1,33 @@
 #! /usr/bin/env python3
 
 if __name__ == '__main__':
-    
     from subprocess import run
     from os import listdir
     from os.path import isdir, exists, abspath, dirname, join
+    import sys
     
+    if len(sys.argv) < 2:
+        print('Please specify extract, update, compile, or all. For example:')
+        print('update_loc.py extract')
+        sys.exit()
+
     SourceTree = dirname(abspath(__file__))
     T = join(SourceTree,'locale')
-    
-    run(f'pybabel extract -F babel.cfg -o {join(T,"pdfstitcher.pot")} --copyright-holder="Charlotte Curtis" --project=pdfstitcher .', shell=True, cwd=SourceTree)
 
-    LOCALES = [lp for lp in listdir(T) if isdir(join(T,lp))]
-    print(LOCALES)
+    if 'extract' in sys.argv or 'all' in sys.argv:
+        run(f'pybabel extract -F babel.cfg -o {join(T,"pdfstitcher.pot")} --copyright-holder="Charlotte Curtis" --project=pdfstitcher .', shell=True)
 
-    for L in LOCALES:
-        invoke_args = f' -D pdfstitcher -d {T} -i {join(T,"pdfstitcher.pot")} -l {L}'
-        if exists(join(T,L,'LC_MESSAGES','pdfstitcher.po')):
-            cmd = 'pybabel update' + invoke_args
-        else:
-            cmd = 'pybabel init' + invoke_args
-        run(cmd, shell=True, cwd=SourceTree)
+    if 'update' in sys.argv or 'all' in sys.argv:
+        LOCALES = [lp for lp in listdir(T) if isdir(join(T,lp))]
+        print(LOCALES)
 
-    run(f'pybabel compile -D pdfstitcher -d {T}', shell=True, cwd=SourceTree)
+        for L in LOCALES:
+            invoke_args = f' -D pdfstitcher -d {T} -i {join(T,"pdfstitcher.pot")} -l {L}'
+            if exists(join(T,L,'LC_MESSAGES','pdfstitcher.po')):
+                cmd = 'pybabel update' + invoke_args
+            else:
+                cmd = 'pybabel init' + invoke_args
+            run(cmd, shell=True)
+
+    if 'compile' in sys.argv or 'all' in sys.argv:
+        run(f'pybabel compile -D pdfstitcher -d locale', shell=True)
