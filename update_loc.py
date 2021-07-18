acca8f2e171dd6f78c4b7a22a84fb3b8ab5f03b5
Charlotte Curtis <c.f.curtis@gmail.com>
2021-07-18 13:55:39 -0600

    Updating locale files and update_loc script for Windows

--- update_loc.py
+++ update_loc.py
@@ -4,22 +4,22 @@ if __name__ == '__main__':
     
     from subprocess import run
     from os import listdir
-    from os.path import isdir, exists, abspath, dirname
+    from os.path import isdir, exists, abspath, dirname, join
     
     SourceTree = dirname(abspath(__file__))
-    T = SourceTree + '/locale/'
+    T = join(SourceTree,'locale')
     
-    run([f'pybabel extract -F babel.cfg -o {T}pdfstitcher.pot --copyright-holder="Charlotte Curtis" --project=pdfstitcher .'], shell=True, cwd=SourceTree)
+    run(f'pybabel extract -F babel.cfg -o {join(T,"pdfstitcher.pot")} --copyright-holder="Charlotte Curtis" --project=pdfstitcher .', shell=True, cwd=SourceTree)
 
-    LOCALES = [lp for lp in listdir(T) if isdir(T+lp)]
+    LOCALES = [lp for lp in listdir(T) if isdir(join(T,lp))]
     print(LOCALES)
 
     for L in LOCALES:
-        invoke_args = f' -D pdfstitcher -d {T} -i {T}pdfstitcher.pot -l {L}'
-        if exists(T+L+'/LC_MESSAGES/pdfstitcher.po'):
+        invoke_args = f' -D pdfstitcher -d {T} -i {join(T,"pdfstitcher.pot")} -l {L}'
+        if exists(join(T,L,'LC_MESSAGES','pdfstitcher.po')):
             cmd = 'pybabel update' + invoke_args
         else:
             cmd = 'pybabel init' + invoke_args
-        run([cmd], shell=True, cwd=SourceTree)
+        run(cmd, shell=True, cwd=SourceTree)
 
-    run([f'pybabel compile -D pdfstitcher -d {T}'], shell=True, cwd=SourceTree)
+    run(f'pybabel compile -D pdfstitcher -d {T}', shell=True, cwd=SourceTree)
