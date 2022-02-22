77deb1c0d4cac786cc7862d7c95938ee2947906f
Charlotte Curtis <ccurtis@mtroyal.ca>
2022-02-22 12:08:22 -0700

    Packaging stuff, working on linux including locale + icon

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 from setuptools import setup
-from pdfstitcher.version import __version__
+from setuptools.command.install import install
+import os
 
-# compile translation messages
-from pdfstitcher.update_loc import compile
-compile()
+# From https://milkr.io/kfei/5-common-patterns-to-version-your-Python-package/5
+version = {}
+with open('pdfstitcher/version.py') as fp:
+    exec(fp.read(), version)
+
+# Shamelessly copied from https://stackoverflow.com/a/41120180
+class InstallWithCompile(install):
+    def run(self):
+        from babel.messages.frontend import compile_catalog
+        compiler = compile_catalog(self.distribution)
+        option_dict = self.distribution.get_option_dict('compile_catalog')
+        compiler.directory = option_dict['directory'][1]
+        compiler.domain = [option_dict['domain'][1]]
+        compiler.run()
+        super().run()
 
 setup(
-    package_data={'': ['locale/*/*/*.mo', 'locale/*/*/*.po']},
-    version = __version__
+    cmdclass={
+        "install": InstallWithCompile
+    },
+    version = version['__version__']
 )
