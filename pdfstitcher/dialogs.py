10ee9d7d364cf55bf69bfed3819f25da5169fc46
Charlotte Curtis <ccurtis@mtroyal.ca>
2022-07-06 16:28:29 -0600

    Dialog for preferences working

--- pdfstitcher/dialogs.py
+++ pdfstitcher/dialogs.py
@@ -7,6 +7,8 @@
 
 import wx
 from pdfstitcher import utils
+from pdfstitcher.utils import Config
+
 
 class PrefsDialog(wx.Dialog):
     """
@@ -16,14 +18,27 @@ class PrefsDialog(wx.Dialog):
     def __init__(self, *args, **kw):
         super(PrefsDialog, self).__init__(*args, **kw)
 
+        self.main_gui = kw["parent"]
         self.SetTitle(_("Preferences"))
         vert_sizer = wx.BoxSizer(wx.VERTICAL)
 
         # Language switcher
         newline = wx.BoxSizer(wx.HORIZONTAL)
         newline.Add(wx.StaticText(self, label=_("Language") + ":"), flag=wx.ALIGN_CENTER_VERTICAL)
-        valid_langs = [lang for lang in gettext.find('pdfstitcher') if lang != 'pdfstitcher']
+        valid_langs = utils.get_valid_langs()
+        self.lang_combo = wx.ComboBox(
+            self, choices=valid_langs, value=Config.general["language"], style=wx.CB_READONLY
+        )
+        newline.Add(
+            self.lang_combo,
+            flag=wx.LEFT | wx.ALIGN_CENTER_VERTICAL,
+            border=self.FromDIP(utils.BORDER),
+        )
 
+        vert_sizer.Add(
+            newline, flag=wx.TOP | wx.LEFT | wx.RIGHT, border=self.FromDIP(utils.BORDER * 2)
+        )
+        self.SetSizer(vert_sizer)
 
 
 class AboutDialog(wx.Dialog):
