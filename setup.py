f2c1954d9933768f5282f4fa5d56490ebf4b7cbf
Charlotte Curtis <ccurtis@mtroyal.ca>
2022-07-06 15:44:36 -0600

    Switching to version in setup.cfg

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup
 from setuptools.command.install import install
-import os
-
-# From https://milkr.io/kfei/5-common-patterns-to-version-your-Python-package/5
-version = {}
-with open('pdfstitcher/version.py') as fp:
-    exec(fp.read(), version)
+from babel.messages.frontend import compile_catalog
 
 # Shamelessly copied from https://stackoverflow.com/a/41120180
 class InstallWithCompile(install):
     def run(self):
-        from babel.messages.frontend import compile_catalog
         compiler = compile_catalog(self.distribution)
         option_dict = self.distribution.get_option_dict('compile_catalog')
         compiler.directory = option_dict['directory'][1]
@@ -22,6 +16,5 @@ class InstallWithCompile(install):
 setup(
     cmdclass={
         "install": InstallWithCompile
-    },
-    version = version['__version__']
+    }
 )
