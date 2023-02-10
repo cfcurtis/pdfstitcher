d75ed9af71e0130704b3737f8640b434ee686661
Charlotte Curtis <ccurtis@mtroyal.ca>
2023-02-09 19:42:43 -0700

    Adding fallback to Czech for Slovak, closes #120. Finally!

--- pdfstitcher/utils.py
+++ pdfstitcher/utils.py
@@ -215,7 +215,12 @@ def setup_locale(lang: str = None) -> None:
     # Then check just the language without country code
     elif lang[:2] in valid_langs:
         lang = lang[:2]
+    elif lang[:2] == "sk":
+        # fallback to Czech if Slovak is not available
+        lang = "cs"
+        language_warning = _("Slovak translation not available, defaulting to Czech")
     else:
+        # check if there's a country code variant for the language
         for valid_lang in valid_langs:
             if lang[:2] in valid_lang:
                 lang = valid_lang
