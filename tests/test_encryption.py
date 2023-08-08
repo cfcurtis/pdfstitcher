50f848a8f01636452ea9cfdbce47c94efe45f18c
Charlotte Curtis <ccurtis@mtroyal.ca>
2023-08-08 15:44:28 -0600

    Trying build on macOS 11, then run

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -1,9 +1,9 @@
 # Issue with legacy openssl on x86 macOS
 
 import pikepdf
-from pathlib import Path
+import sys
 
-test_fname = Path(__file__).parent / "encryption_test.pdf"
+test_fname = sys.argv[1]
 doc = pikepdf.open(test_fname, password="encryption_test")
 
 print("Successfully opened encrypted PDF")
