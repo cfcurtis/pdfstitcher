49353fce4f3213519b0560ba095d341fabe0feea
Charlotte Curtis <ccurtis@mtroyal.ca>
2024-06-18 11:03:16 -0600

    Fixing order of build

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from pathlib import Path
 from setuptools import setup
 from setuptools.command.install import install
 from setuptools.command.develop import develop
@@ -9,6 +10,10 @@ def run_compile(cmdclass):
     # Shamelessly copied from https://stackoverflow.com/a/41120180
     compiler = compile_catalog(cmdclass.distribution)
     compiler.directory = "resources/locale"
+    compiler.directory = "pdfstitcher/resources/locale"
+    if not Path(compiler.directory).exists():
+        print("warning: no locale directory found, only English will be available.")
+        return
     compiler.domain = ["pdfstitcher"]
     compiler.run()
 
